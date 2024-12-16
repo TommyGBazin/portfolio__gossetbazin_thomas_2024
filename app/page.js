f69e2f1afa9710eb27981aff15c71a9f40504958
TommyGBazin <thomasgossetbazin@gmail.com>
2024-12-16 09:38:06 +0100

    Second commit

--- app/page.js
+++ app/page.js
@@ -1,14 +1,23 @@
-async function simulateLoading() {
-  return new Promise((resolve) => setTimeout(resolve, 3000));
-}
+'use client';
 
-export default async function HomePage() {
-  await simulateLoading(); // Simule 3 secondes de chargement
-
+export default function HomePage() {
   return (
-    <main>
-      <h1>Bienvenue sur mon site</h1>
-      <p>Ceci est la page d'accueil.</p>
-    </main>
+    <section className="grid grid-rows-3">
+      <div className="col-span-2 grid grid-cols-2 items-center">
+        <h2 className="text-sm text-gray-300 tracking-widest">{'('} <span className="px-2">designer</span> {')'}</h2>
+        <h2 className="text-sm text-gray-300 tracking-widest">{'('} <span className="px-2">developer</span> {')'}</h2>
+      </div>
+      <div className="col-span-2 flex flex-row w-fit items-center text-base">
+        <h1>
+          <span>thomas</span>
+          <span>gosset</span>
+          <span>bazin</span>
+        </h1>
+        <span>@gmail.com</span>
+      </div>
+      <div>
+        <h3 className="text-sm text-gray-300 tracking-widest">{'('} <span className="px-2">Lyon, France</span> {')'}</h3>
+      </div>
+    </section>
   );
 }
